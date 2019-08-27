f2a1c28309db89895f1857eeadf6e7096afbab4a
Priyanka Sharma <priyanka353rocks@gmail.com>
2019-08-28 00:28:20 +0800

    Update insertsort.py
    hacker rank challenge

--- hackerearth/insertsort.py
+++ hackerearth/insertsort.py
@@ -1,1 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
 import sys
+
+# Complete the insertionSort1 function below.
+def insertionSort1(n, arr):
+    n=int(n)
+    temp=arr[n-1]
+    for i in reversed(range(n-1)):
+        j=i-1
+        if arr[i] > temp :
+            arr[i+1] = arr[i]
+            print(*arr,sep=' ')
+            if j < 0:
+                arr[i]=temp
+                print(*arr,sep=' ')
+                break
+            j = j-1
+        else:
+            arr[i+1]=temp
+            print(*arr,sep=' ')
+            break
+        
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
